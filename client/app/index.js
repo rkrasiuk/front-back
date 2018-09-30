21cdfda15a5d778cd6ae9d4f0d087367755a63bb
Roman Krasiuk <rokrassyuk@gmail.com>
2018-09-30 13:48:19 +0300

    add form for adding goods

--- client/app/index.js
+++ client/app/index.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
 
+const initialForm = {
+
+};
+
 class App extends Component {
+  state = {
+    form: initialForm,
+  };
+
+  updateFormField = (key, value) => this.setState({form: {...this.state.form, [key]: value}});
+
   render() {
+    const form = this.state;
+    const {
+      vendorCode, name, brand, price,
+    } = form;
+
     return (
       <div className="app">
-        Congrats! You are logged in.
+        <form className="add-goods-form">
+          <input
+            type="text"
+            placeholder="Vendor Code"
+            value={vendorCode}
+            onChange={value => this.updateFormField('vendorCode', value)}
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={value => this.updateFormField('name', value)}
+          />
+          <input
+            type="text"
+            placeholder="Brand"
+            value={brand}
+            onChange={value => this.updateFormField('brand', value)}
+          />
+          <input
+            type="text"
+            placeholder="Price"
+            value={price}
+            onChange={value => this.updateFormField('price', value)}
+          />
+        </form>
       </div>
     );
   }
--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,21 @@
-Meteor.methods({
+import SimpleSchema from 'simpl-schema';
+import Goods from 'collections/goods';
 
+Meteor.methods({
+  'goods.addGood': ({
+    vendorCode, name, brand, price,
+  }) => {
+    new SimpleSchema({
+      vendorCode: {type: Number},
+      name: {type: String},
+      brand: {type: String},
+      price: {type: Number},
+    }).validate({
+      vendorCode, name, brand, price,
+    });
+
+    return Goods.insert({
+      vendorCode, name, brand, price,
+    });
+  },
 });
