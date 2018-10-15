ab7f76d5e4fb96fcd9d74fe6931df95f307e06cc
Roman Krasiuk <rokrassyuk@gmail.com>
2018-10-15 23:39:21 +0300

    parse price from website

--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,10 @@
+/* eslint-disable no-useless-escape */
 import SimpleSchema from 'simpl-schema';
+import axios from 'axios';
+import {parse} from 'node-html-parser';
+import isUrl from 'is-url';
+import moment from 'moment';
+
 import Goods from 'collections/goods';
 import Competitors from 'collections/competitors';
 import CompetitorGoods from 'collections/competitorgoods';
@@ -47,14 +53,29 @@ Meteor.methods({
     return Competitors.remove({_id});
   },
 
-  'competitorgoods.addCompetitorGood': ({goodId, competitorId, url}) => {
+  'competitorgoods.addCompetitorGood': async ({goodId, competitorId, url}) => {
     new SimpleSchema({
       goodId: {type: String},
       competitorId: {type: String},
       url: {type: String},
     }).validate({goodId, competitorId, url});
 
-    return CompetitorGoods.insert({goodId, competitorId, url});
+    if (!isUrl(url)) {
+      throw new Meteor.Error('Not a valid url');
+    }
+
+    CompetitorGoods.insert({goodId, competitorId, url});
+
+    // 'https://www.mobilluck.com.ua/katalog/naboru_kastrul/peterhof/peterhof-PH-15136-91958.html'
+    const response = await axios.get(url);
+    const root = parse(response.data);
+    const htmlPrice = root.querySelector('.price');
+    const price = htmlPrice && htmlPrice.firstChild.text && parseInt(htmlPrice.firstChild.text.split(' ').join(''));
+    console.log(price);
+
+    return ParsedGoods.insert({
+      time: moment().format(), goodId, competitorId, price,
+    });
   },
 
   'competitorgoods.removeCompetitorGood': ({_id}) => {
