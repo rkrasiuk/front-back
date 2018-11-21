50cb3c64a1aa8f1c6fc119b38d39910ddcd4c284
Roman Krasiuk <rokrassyuk@gmail.com>
2018-11-21 11:20:44 +0200

    style nav bar

--- client/app/components/Navigation/index.js
+++ client/app/components/Navigation/index.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
-import {NavLink} from 'react-router';
+import {NavLink} from 'react-router-dom';
 
-class Navigation extends Component {
+class NavigationBar extends Component {
   render() {
     return (
       <div className="navigation">
-        <h1>Dream Team</h1>
-        <div className="links">
-          <NavLink to="/goods">Goods</NavLink>
-          <NavLink to="/competitors">Competitors</NavLink>
-        </div>
+        <h3 className="navigation-title">Dream Team</h3>
+        <nav className="links">
+          <NavLink activeStyle={{color: '#7887E8'}} className="nav-link" to="/goods">Goods</NavLink>
+          <NavLink activeStyle={{color: '#7887E8'}} className="nav-link" to="/competitors">Competitors</NavLink>
+          <NavLink activeStyle={{color: '#7887E8'}} className="nav-link" to="/report">Report</NavLink>
+        </nav>
       </div>
     );
   }
 }
 
-export default Navigation;
+export default NavigationBar;
