2293c060e4ee8558b7f1d6f008558766f533612f
Roman Krasiuk <rokrassyuk@gmail.com>
2018-11-21 13:21:32 +0200

    merge db abstractions into two only

--- imports/collections/index.js
+++ imports/collections/index.js
@@ -1,4 +1,28 @@
+import SimpleSchema from 'simpl-schema';
+
 const Competitors = new Mongo.Collection('competitors');
 const Goods = new Mongo.Collection('goods');
+
+const goodSchema = new SimpleSchema({
+  goodId: String,
+  vendorCode: Number,
+  name: String,
+  brand: String,
+  price: Number,
+});
+Goods.attachSchema(goodSchema);
+
+const competitorSchema = new SimpleSchema({
+  name: String,
+  parsingRules: String,
+  goods: Array,
+  'goods.$': Object,
+  'goods.$.goodId': String,
+  'goods.$.url': String,
+  'goods.$.parsed': Boolean,
+  'goods.$.time': String,
+  'goods.$.price': String,
+});
+Competitors.attachSchema(competitorSchema);
 
 export default {Competitors, Goods};
