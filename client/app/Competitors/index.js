952167eed60eb7dd2871c0f1287dbdd56516945b
Roman Krasiuk <rokrassyuk@gmail.com>
2018-11-22 02:45:54 +0200

    create competitors page

---
+++ client/app/Competitors/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+
+import Table from 'components/Table';
+
+import Header from '../components/Header';
+import NavigationBar from '../components/Navigation';
+
+import './index.scss';
+
+class CompetitorsPage extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header>
+          <Button onClick={this.handleAddClick} variant="fab" color="primary" aria-label="Add" className="edit-button">
+            <AddIcon />
+          </Button>
+          <Button variant="fab" color="primary" aria-label="Edit" className="edit-button">
+            <Icon>edit_icon</Icon>
+          </Button>
+        </Header>
+        <div className="content">
+          <NavigationBar activeLink={this.props.match.url} />
+          <div className="goods-table">
+            <Table />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CompetitorsPage;
--- client/router.js
+++ client/router.js
@@ -3,14 +3,16 @@ import {
   BrowserRouter as Router, Route, Switch, Redirect,
 } from 'react-router-dom';
 
-import GoodsPage from './app/Goods';
+import Goods from './app/Goods';
+import Competitors from './app/Competitors';
 
 class AppRouter extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/goods" component={GoodsPage} />
+          <Route path="/goods" component={Goods} />
+          <Route path="/competitors" component={Competitors} />
           <Redirect from="*" to="/goods" />
         </Switch>
       </Router>
